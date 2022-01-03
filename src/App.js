4e0f596cac8308d07d0a258661dcc1a1bdfc4a1c
sundar17 <sundar@swivt.com.np>
2022-01-03 20:36:36 +0545

    statemanagement in react completed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
- 
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+
+    this.state = { count: 0 }; //state create gare
   }
 
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
 
   render() {
     return (
       <div className="main-app">
-        {/*This will contains code for Jan-3 2022 */}
-    
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
 }
-
-export default App;
+export default App; //this will help us to import in another component
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="course" />
   </React.StrictMode>,
   document.getElementById("root")
 );
