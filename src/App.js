d1e9a2abc1a01f96bbb93e57f8b2e4c87226be7d
sundar17 <sundar@swivt.com.np>
2022-01-06 18:15:53 +0545

    jan-5-sundar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="main-app">
-        
+      <div>
+        <h1 style={{
+          textAlign:"center"
+        }}>Want to move forward to Functional Component?</h1>
+
+        <img style={{
+          height:"50vh",
+          width:"50vw",
+          objectFit:"contain",
+          position:"absolute",
+          left:"20vw",
+          top:"20vh"
+        }} src="https://www.pngkey.com/png/full/96-965627_confused-person-png-animated-confused-man-clipart-png.png" alt="" />
+       
+       <marquee> <b style={{
+          color:"red",
+          fontSize:"50px",
+
+        }}>If any confusion till now please don't feel shy to share it.We are here to learn</b></marquee>
       </div>
     );
   }
