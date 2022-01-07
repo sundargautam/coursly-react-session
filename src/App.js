eda6b1c55bd34c98eda4511d7e9148204420b7b8
sundar17 <sundar@swivt.com.np>
2022-01-07 18:33:13 +0545

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-//eventlistening,props,statemangement,lifecycle
 
 export const App = ({ name, description }) => {
   return (
     <div className="main-app">
-      <h1>Hello world {name}</h1>
-      <h2>{description}</h2>
+  
     </div>
   );
 };
