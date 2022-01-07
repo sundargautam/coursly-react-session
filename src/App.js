2129917d9586f4f47571f145426e900ef99fac65
sundar17 <sundar@swivt.com.np>
2022-01-07 20:17:33 +0545

    jan-7-sundar talked about state management using hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from "react";
+import { useState } from "react";
+const App = () => {
+  const [info, setInfo] = useState({name:"",age:0,class:8,address:{
+    city:{
+      zip:"ss",
+      postal:""
+    }
+  }});
 
-class App extends Component {
-  constructor(props) {
-    super(props);
- 
-  }
+  return (
+    <div className="main-app">
+      <h2>Name: {info.name} </h2>
+      <h1>Age:{info.age}</h1>
+      <h3>{info.class}</h3>
+      <button
+       onClick={(e)=>{
+            setInfo(prevState=>({...prevState,name:"xyz"}))
 
-
-  render() {
-    return (
-      <div className="main-app">
-    
-      </div>
-    );
-  }
-}
+         
+        }} >Change Name</button>
+      {/* <button onClick={}>Change Age</button> */}
+    </div>
+  );
+};
 
 export default App;
