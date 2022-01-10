c14eacfbc3642a3f67d108433e8364f88ab7cac6
sundar17 <sundar@swivt.com.np>
2022-01-10 17:35:25 +0545

    modified main

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
- 
-  }
-
-
-  render() {
-    return (
-      <div className="main-app">
-    
-      </div>
-    );
-  }
+const App = ()=>{
+  return(
+    <div className="main-app">
+      
+    </div>
+  )
 }
 
 export default App;
