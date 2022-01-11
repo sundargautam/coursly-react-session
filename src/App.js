72374a80ec1035159ec644872d9d098a10c16be1
sundar17 <sundar@swivt.com.np>
2022-01-11 20:34:00 +0545

    jan-11 birthdayremainder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from "react";
-const App = ()=>{
-  return(
-    <div className="main-app">
-      
-    </div>
-  )
-}
+import React, { Component, useEffect, useState } from "react";
+import List from "./component/List";
+import data from "./seed";
+const App = () => {
+  const [userDetails, setUserDetails] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{userDetails.length} birthdays today</h3>
+        <List people={userDetails} />
+        <button onClick={() => setUserDetails([])}>clear all</button>
+      </section>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/seed.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: "AnushaG",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Bipan Gautam",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Saroj Bhandari",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Kailash Kher",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Sruti Chinta",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
+
+export default data;
